11cf16c34a932249c028f4d0cb21d6720b04b769
CrimsonShadow <groffb@roverkids.org>
2024-12-04 14:15:13 -0500

    Finished 2.3

--- BasicGameplay/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplay/Assets/Scripts/DestroyOutOfBounds.cs
@@ -13,12 +13,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > 30)
-        {
-            Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
